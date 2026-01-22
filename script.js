499cedf83d07458218826cc2f6e315ce0da7abaf
WWZ <33089305+wangwenzhiwwz@users.noreply.github.com>
2026-01-22 22:04:55 +0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", async () => {
-    const response = await fetch("images.json");
-    const images = await response.json();
-    const gallery = document.getElementById("gallery");
-
-    gallery.innerHTML = images.map(img => `<img src="${img}" loading="lazy">`).join("");
-});
